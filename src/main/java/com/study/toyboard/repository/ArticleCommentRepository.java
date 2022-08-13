6c1d21490c105db705f4518bc4f2a8382726def7
jeonghoonb <bangdjh2@gmail.com>
2022-08-14 07:56:11 +0900

    [#3] DB 초기 세팅 * 사용하지 않는 test db yml 설정 제거 * 일부 파일 eof new line 추가

--- src/main/java/com/study/toyboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/study/toyboard/repository/ArticleCommentRepository.java
@@ -4,4 +4,5 @@ import com.study.toyboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+
 }
--- src/main/java/com/study/toyboard/repository/ArticleRepository.java
+++ src/main/java/com/study/toyboard/repository/ArticleRepository.java
@@ -3,5 +3,12 @@ package com.study.toyboard.repository;
 import com.study.toyboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/*
+    @Repository 어노테이션 붙이지 않는다.
+    JpaRepository 구현체인 SimpleJpaRepository에 붙어 있다.
+
+    그리고 일반적으로 StereoType 어노테이션은 Interface가 아니라 구현 Class에 붙인다.
+ */
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+
 }
