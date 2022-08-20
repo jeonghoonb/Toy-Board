51d0f03385a39c861cd6e1a6341fb3227871f8f5
jeonghoonb <bangdjh2@gmail.com>
2022-08-20 14:25:58 +0900

    [#2] DB 초기 세팅 * Entity 공통 Column (createdAt/By, modifiedAt/By) Auditing 기능으로 추출 * 사용하지 않는 h2-console yml 제거

--- src/main/java/com/study/toyboard/domain/ArticleComment.java
+++ src/main/java/com/study/toyboard/domain/ArticleComment.java
@@ -4,14 +4,8 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
