4a2efdb50010e91c30c73e340e6f85c45f898e95
jeonghoonb <bangdjh2@gmail.com>
2022-08-20 14:53:12 +0900

    [#3] API 개발 * Spring Data Rest TC 작성

---
+++ src/test/java/com/study/toyboard/controller/DataRestTest.java
@@ -0,0 +1,38 @@
+package com.study.toyboard.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("Spring Data Rest 테스트")
+@Transactional  // NOTE: @SpringBootTest는 Integration Test라서 실제 DB에 영향을 준다. 그렇기에 @Transactional 추가 (Test 에서 동작하는 Transactional은 Default Rollback)
+@AutoConfigureMockMvc
+@SpringBootTest
+public class DataRestTest {
+
+    private final MockMvc mvc;
+
+    public DataRestTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("게시글 리스트 조회 테스트")
+    @Test
+    void given_whenRequestArticles_thenReturnArticles() throws Exception {
+        // given
+
+        // when & then
+        mvc.perform(get("/api/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json ")));
+    }
+}
