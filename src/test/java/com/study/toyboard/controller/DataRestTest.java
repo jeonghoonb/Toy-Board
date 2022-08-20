25752c96a8e172299e3130775e14b4aac092b7b6
jeonghoonb <bangdjh2@gmail.com>
2022-08-20 14:58:43 +0900

    [#3] API 개발 * Spring Data Rest TC Disable 처리

--- src/test/java/com/study/toyboard/controller/DataRestTest.java
+++ src/test/java/com/study/toyboard/controller/DataRestTest.java
@@ -1,5 +1,6 @@
 package com.study.toyboard.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Disabled("Spring Data Rest가 자동으로 생성해주기에, 이 테스트 통합 테스트는 사실상 불필요. 그냥 스터디용도로 진행했기에 Disable")
 @DisplayName("Spring Data Rest 테스트")
 @Transactional  // NOTE: @SpringBootTest는 Integration Test라서 실제 DB에 영향을 준다. 그렇기에 @Transactional 추가 (Test 에서 동작하는 Transactional은 Default Rollback)
 @AutoConfigureMockMvc
