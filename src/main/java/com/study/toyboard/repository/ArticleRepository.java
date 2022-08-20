0ef679c4b9747e4bc7bb28233e7e2e29e4228d36
jeonghoonb <bangdjh2@gmail.com>
2022-08-20 14:37:21 +0900

    [#3] API 개발 * data-rest, hal-explorer Gradle Dependency 추가 * yaml에 data rest 설정 추가 * API로 노출 시킬 Repository에 Annotation 추가

--- src/main/java/com/study/toyboard/repository/ArticleRepository.java
+++ src/main/java/com/study/toyboard/repository/ArticleRepository.java
@@ -2,6 +2,7 @@ package com.study.toyboard.repository;
 
 import com.study.toyboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /*
     @Repository 어노테이션 붙이지 않는다.
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
     그리고 일반적으로 StereoType 어노테이션은 Interface가 아니라 구현 Class에 붙인다.
  */
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
 }
