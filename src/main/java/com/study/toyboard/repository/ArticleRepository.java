4e9492c28d5c2158cf03a8615be0a67a5dfe8117
jeonghoonb <bangdjh2@gmail.com>
2022-08-20 16:13:58 +0900

    [#3] API 개발 * Querydsl 지원 기능(QuerydslPredicateExecutor, QuerydslBinderCustomizer)을 사용하여 API 검색 옵션 커스터마이징

--- src/main/java/com/study/toyboard/repository/ArticleRepository.java
+++ src/main/java/com/study/toyboard/repository/ArticleRepository.java
@@ -1,7 +1,13 @@
 package com.study.toyboard.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import com.study.toyboard.domain.Article;
+import com.study.toyboard.domain.QArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /*
@@ -11,6 +17,22 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
     그리고 일반적으로 StereoType 어노테이션은 Interface가 아니라 구현 Class에 붙인다.
  */
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle>
+{
 
+    // NOTE: java 8 이후 Interface에 default method 가능, 별도 Repository 구현체 만들지 않고 Spring Data JPA를 이용
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true);   // listing 하지 않은 컬럼에 대해 검색 결과 제외
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+//        bindings.bind(root.title).first(StringExpression::likeIgnoreCase);  // like '${param}'  -> % 개발자가 추가해야
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);   // like '%${param}%'
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);   // like '%${param}%'
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);   // like '%${param}%'
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
